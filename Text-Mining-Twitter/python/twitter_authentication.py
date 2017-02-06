d6bc3cbb1adcba5db15edab22bdd7489e493adfc
Eric <ericchang00@gmail.com>
2017-02-06 02:47:02 -0600

    add twitter text mining V1

---
+++ Text-Mining-Twitter/python/twitter_authentication.py
@@ -0,0 +1,11 @@
+import twitter
+from python.API_KEYS_TWITTER import *
+
+def authenticate_twitter():
+	"""
+	returns an authenticated Twitter object to access the API
+	"""
+	auth = twitter.oauth.OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET,
+	                           CONSUMER_KEY, CONSUMER_SECRET)
+
+	return twitter.Twitter(auth=auth)
