3c40f72306a76a367eb7a2165b7e35131f46d14b
Eric Chang <eric@Erics-MacBook-Pro.local>
2017-04-19 22:52:34 -0500

    fix whitespace

--- Text-Mining-Twitter/twitter_helpers/authentication.py
+++ Text-Mining-Twitter/twitter_helpers/authentication.py
@@ -1,11 +1,12 @@
 import twitter
 from twitter_helpers.API_KEYS_TWITTER import *
 
-def authenticate_twitter():
-	"""
-	returns an authenticated Twitter object to access the API
-	"""
-	auth = twitter.oauth.OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET,
-	                           CONSUMER_KEY, CONSUMER_SECRET)
 
-	return twitter.Twitter(auth=auth)
+def authenticate_twitter():
+    """
+    returns an authenticated Twitter object to access the API
+    """
+    auth = twitter.oauth.OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET,
+                               CONSUMER_KEY, CONSUMER_SECRET)
+
+    return twitter.Twitter(auth=auth)
